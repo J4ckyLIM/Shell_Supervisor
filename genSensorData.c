0f0a9121b992f02139650feae4f1d959385be0d2
Jacky LIM <jacky.lim@edu.itescia.fr>
2020-01-15 11:40:16 +0100

    Story 4 completedand documented

--- genSensorData.c
+++ genSensorData.c
@@ -8,7 +8,18 @@ int main()
 
     while (read(0, &c, 1) == 1)
     {
-        write(1, &c, 1);
+        fflush(stdout);
+        // Si il recoit "OK" on envoit un message
+        if(c ==  'K'){
+            printf("Everything is OK\n");
+        }
+        // Si on recoit "ERROR"
+        else if(c == 'E'){
+            printf("An error occured\n");
+        }
+        else{
+            printf("Nothing happened");
+        }
     }
     //printf(stdin, stdout,"\n");
 }
