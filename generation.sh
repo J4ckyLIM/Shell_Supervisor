8c68bff79ea2375f59b4c76e2f7b8c3ec63a5720
Jacky LIM <jacky.lim@edu.itescia.fr>
2020-01-15 16:35:44 +0100

    Story 7: Getting user

--- generation.sh
+++ generation.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./genExec $1 | ./genSensorData 2> err.txt > log.txt
+vraiUser="limjacky"
+user=$USER
+if [ $user = $vraiUser ]
+then
+    ./genExec $1 | ./genSensorData 2> err.txt > log.txt
+else
+    echo "Vous n'êtes pas le propriétaire du Mac, le processus s'arrête"
+fi
