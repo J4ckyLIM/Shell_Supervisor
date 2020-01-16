4a68d85b7633c120576adc78a6f73b10e850ec96
Jacky LIM <jacky.lim@edu.itescia.fr>
2020-01-16 11:11:50 +0100

    Error messages, sensor Name added

--- genSensorData.c
+++ genSensorData.c
@@ -5,6 +5,20 @@
 
 char c;
 char buffer[120];
+char* sensorName[]= {
+    "Détecteur de fumé",
+    "Capteur d'humidité",
+    "Capteur de mouvement",
+    "Détecteur d'ouverture de porte"
+};
+
+char* errMsg[] = {
+    "Capteur non fonctionnel",
+    "Batterie vide",
+    "Erreur système",
+    "Branchement raté",
+    "Erreur paramétrage"
+};
 
 int main()
 {
@@ -15,12 +29,12 @@ int main()
         fflush(stdout);
         // Si il recoit "OK" on envoit un message
         if(c ==  'K'){
-            sprintf(buffer, "SensorID: %d; SensorName: %s; Value: %d; MinValue: %d; MeanValue: %d; MaxValue: %d\n", rand() % 10000, "Capteur", rand() % 100, rand() % 10, rand() % 50, rand() % 100);
+            sprintf(buffer, "SensorID: %d; SensorName: %s; Value: %d; MinValue: %d; MeanValue: %d; MaxValue: %d\n", rand() % 10000, sensorName[rand() % 4], rand() % 100, rand() % 10, rand() % 50, rand() % 100);
             write(1,buffer,strlen(buffer));
         }
         // Si on recoit "ERROR"
         else if(c == 'E'){
-            sprintf(buffer, "SensorID: %d; errorCode: %d; errorDetails: %s\n", rand() % 10000, rand() %100, "Le capteur n'est plus en marche");
+            sprintf(buffer, "SensorID: %d; errorCode: %d; errorDetails: %s\n", rand() % 10000, rand() %100, errMsg[rand() % 5]);
             write(2,buffer,strlen(buffer));
         }
     }
