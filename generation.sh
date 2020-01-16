cfe9675b3c7a9fe2557fe00c1416e2fb03907171
Jacky LIM <jacky.lim@edu.itescia.fr>
2020-01-16 10:53:32 +0100

    Filtering ready, write in log need to be implemented

--- generation.sh
+++ generation.sh
@@ -2,9 +2,10 @@
 
 vraiUser="limjacky"
 user=$USER
+# Checking if the user is the owner , if No , the process stops
 if [ $user = $vraiUser ]
 then
-    ./genExec $1 | ./genSensorData 2> err.txt > log.txt
+    ./genExec $1 | ./genSensorData 2> err.txt | stdbuf -oL cut -d ";" -f1,2,3,6  > log.txt 
 else
     echo "Vous n'êtes pas le propriétaire du Mac, le processus s'arrête"
 fi
